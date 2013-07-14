b73f2d283bd03703b49ca8cd8777fb8dbe69bea6
Taiki ONO <taiks.4559@gmail.com>
2013-07-14 16:31:35 +0900

    add minium eval

--- eval.rb
+++ eval.rb
@@ -1,3 +1,6 @@
+require 'pry'
+
+
 def _eval(exp)
   if not list? exp
     if immediate_val? exp
@@ -11,3 +14,48 @@ def _eval(exp)
     apply(func, args)
   end
 end
+
+def list?(exp)
+  exp.is_a? Array
+end
+
+def lookup_primitive_func(exp)
+  $primitive_func_env[exp]
+end
+
+$primitive_func_env = {
+  :+ => [:prim, lambda {|x, y| x + y }],
+  :- => [:prim, lambda {|x, y| x + y }]
+}
+
+def car(list)
+  list[0]
+end
+
+def cdr(list)
+  list[1..-1]
+end
+
+def eval_list(exps)
+  exps.map {|e| _eval(e) }
+end
+
+def immediate_val?(exp)
+  num?(exp)
+end
+
+def num?(exp)
+  exp.is_a? Numeric
+end
+
+def apply(func, args)
+  apply_primitive_func(func, args)
+end
+
+def apply_primitive_func(func, args)
+  func = func[1]
+  func.call(*args)
+end
+
+
+binding.pry
